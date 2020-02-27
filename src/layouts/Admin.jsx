8947afd924cf87e2fdb03674db1cdf36a60e853e
Eduardo Vieira <edusvieirap@gmail.com>
2020-02-27 10:18:35 -0300

    .

--- src/layouts/Admin.jsx
+++ src/layouts/Admin.jsx
@@ -1,20 +1,4 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import NotificationSystem from "react-notification-system";
@@ -180,7 +164,7 @@ class Admin extends Component {
             brandText={this.getBrandText(this.props.location.pathname)}
           />
           <Switch>{this.getRoutes(routes)}</Switch>
-          <Footer />
+
           
         </div>
       </div>
--- src/views/Dashboard.jsx
+++ src/views/Dashboard.jsx
@@ -1,37 +1,27 @@
 import React, { Component } from "react";
-import ChartistGraph from "react-chartist";
-import { Grid, Row, Col } from "react-bootstrap";
+import { Grid, Row, Col, form, FormGroup, ControlLabel, HelpBlock, FormControl} from "react-bootstrap";
 
-import { Card } from "components/Card/Card.jsx";
-import { StatsCard } from "components/StatsCard/StatsCard.jsx";
-import { Tasks } from "components/Tasks/Tasks.jsx";
-import {
-  dataPie,
-  legendPie,
-  dataSales,
-  optionsSales,
-  responsiveSales,
-  legendSales,
-  dataBar,
-  optionsBar,
-  responsiveBar,
-  legendBar
-} from "variables/Variables.jsx";
 
 import { Player } from 'video-react';
 import "../../node_modules/video-react/dist/video-react.css";
+import Button from 'components/CustomButton/CustomButton.jsx';
+
+
+function FieldGroup({ id, label, help, ...props }) {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
 
 class Dashboard extends Component {
 
-  createLegend(json) {
-    var legend = [];
-    for (var i = 0; i < json["names"].length; i++) {
-      var type = "fa fa-circle text-" + json["types"][i];
-      legend.push(<i className={type} key={i} />);
-      legend.push(" ");
-      legend.push(json["names"][i]);
-    }
-    return legend;
+  constructor(props) {
+    super(props);
+    this.state = {isLoading: false};
   }
 
   play = () => {
@@ -62,33 +52,80 @@ class Dashboard extends Component {
   }
 
   render() {
-    return (
-      <div className="content">
-        <Grid fluid>
-          
-          <Row>
-            <Col md={12}>
-              <Player
-                ref={player => {
-                  this.player = player;
-                }}
-                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-              />
+    const isLoading = this.state.isLoading;
 
-            <button onClick={this.play}>Tocar</button>
-            <button onClick={this.pause}>Pausar</button>
-            <button onClick={this.changeCurrentTime(10)}>Avançar</button>
-            <button onClick={this.changeCurrentTime(-10)}>Retroceder</button>
-            <button onClick={this.changePlaybackRateRate(-0.7)}>Lento</button>
-            <button onClick={this.changePlaybackRateRate(0)}>Normal</button>
-            <button onClick={this.changePlaybackRateRate(0.7)}>Rápido</button>
-                       
-            </Col>
+    if(isLoading) {
+      return (
+        <div className="content">
+          <Grid fluid>
             
-          </Row>
-        </Grid>
-      </div>
-    );
+            <Row>
+              <Col md={12}>
+
+              <form>
+                <FieldGroup
+                  id="formControlsFile"
+                  type="file"
+                  label="Carregar vídeo"
+                  help="Por favor, carregue um vídeo em formato MP4 para processamento."
+                />
+              </form>
+                
+              </Col> 
+            </Row>
+  
+          </Grid>
+        </div>
+      )
+    } else {
+      return (
+        <div className="content">
+          <Grid fluid>
+            
+            <Row>
+              <Col md={10}>
+                <Player
+                  ref={player => {
+                    this.player = player;
+                  }}
+                  src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                />
+                         
+              </Col>  
+  
+              <Col md={2}>
+                <Row>
+                  <Button bsStyle="primary" onClick={this.play} fill block>Tocar</Button>
+                </Row>
+                <Row>
+                  <Button bsStyle="primary" onClick={this.pause} fill block>Pausar</Button>
+                </Row>
+                <Row>
+                  <Button bsStyle="primary" onClick={this.changeCurrentTime(10)} fill block>Avançar</Button>
+                </Row>
+                <Row>
+                  <Button bsStyle="primary" onClick={this.changeCurrentTime(-10)} fill block>Retroceder</Button>
+                </Row>
+  
+                <Row>
+                  <Button bsStyle="primary" onClick={this.changePlaybackRateRate(-0.7)} fill block>Lento</Button>
+                </Row>
+  
+                <Row>
+                  <Button bsStyle="primary" onClick={this.changePlaybackRateRate(0)} fill block>Normal</Button>
+                </Row>
+  
+                <Row>
+                  <Button bsStyle="primary" onClick={this.changePlaybackRateRate(0.7)} fill block>Rápido</Button>
+                </Row>
+              </Col> 
+            </Row>
+  
+          </Grid>
+        </div>
+      );
+
+    }
   }
 }
 
